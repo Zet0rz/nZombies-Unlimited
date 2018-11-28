ac87596fcf90e7caac3fe1c51c01f014ebf82326
Zet0rz <zet0rmail2@gmail.com>
2018-11-29 00:40:45 +0100

    Menues, Submenues, Buttons
    - You can now add buttons to the menu
    - You can now add Sub menues to the menu (and other sub menues)
    - You can now add custom panels to the menu
    - Added styling to the menu
    - Made menu clickable
    - Adjusted padding and docking a bit

--- gamemodes/nzombies-unlimited/gamemode/menu.lua
+++ gamemodes/nzombies-unlimited/gamemode/menu.lua
@@ -1,34 +1,165 @@
 
 if CLIENT then
+	local SUBMENU = {}
+	local cornerthickness = 3
+	local cornerlength = 15
+	local textcolor = Color(220,220,220)
+	local paintfunc = function(b, w, h) -- Draw the black button with the white corners
+		if b.Hovered then
+			surface.SetDrawColor(50,50,50,240)
+		else
+			surface.SetDrawColor(0,0,0,220)
+		end
+		b:DrawFilledRect()
+
+		surface.SetDrawColor(255,255,255, 30)
+		surface.DrawRect(0,0,cornerlength, cornerthickness)
+		surface.DrawRect(0,cornerthickness, cornerthickness, cornerlength - cornerthickness)
+
+		surface.DrawRect(w - cornerlength,0,cornerlength, cornerthickness)
+		surface.DrawRect(w - cornerthickness,cornerthickness, cornerthickness, cornerlength - cornerthickness)
+
+		surface.DrawRect(0,h - cornerthickness,cornerlength, cornerthickness)
+		surface.DrawRect(0,h - cornerlength, cornerthickness, cornerlength - cornerthickness)
+
+		surface.DrawRect(w - cornerlength,h - cornerthickness,cornerlength, cornerthickness)
+		surface.DrawRect(w - cornerthickness,h - cornerlength, cornerthickness, cornerlength - cornerthickness)
+	end
+	local function generatebutton(text)
+		local b = vgui.Create("DButton")
+		b:SetFont("DermaLarge")
+		b:SetText(" " .. text .. " ") -- Append spaces around to free from edges
+		b:SetTextColor(textcolor)
+		b:SetContentAlignment(4)
+		b:DockMargin(1,1,1,1)
+		b:SetTall(50)
+
+		b.Paint = paintfunc
+
+		return b
+	end
+	function SUBMENU:AddButton(text, zpos, func)
+		if IsValid(self.List) then
+			local b = generatebutton(text)
+			b:SetZPos(zpos)
+			b.SubMenu = self
+			b.DoClick = func
+			self.List:Add(b)
+		end
+	end
+
+	local function submenuclick(b)
+		local s = b.SubMenu
+		if IsValid(s) then
+			local p = b.SubPanel
+			if IsValid(p) then
+				s:Hide()
+				p:Show()
+			end
+		end
+	end
+	function SUBMENU:AddSubMenu(text, zpos)
+		if IsValid(self.List) then
+			local b = generatebutton(text)
+			b:SetZPos(zpos)
+
+			local submenu = vgui.Create("nzu_MenuPanel_SubMenu")
+			submenu:SetParent(self:GetParent())
+			submenu:Dock(FILL)
+			submenu.Menu = self.Menu
+
+			b.SubMenu = self
+			b.SubPanel = submenu
+			submenu:SetPrevious(text, self)
+			b.DoClick = submenuclick
+			self.List:Add(b)
+
+			return submenu
+		end
+	end
+	function SUBMENU:AddPanel(text, zpos, panel)
+		if IsValid(self.List) then
+			local b = generatebutton(text)
+			b:SetZPos(zpos)
+
+			local submenu = vgui.Create("nzu_MenuPanel_SubMenu")
+			submenu:SetParent(self:GetParent())
+			submenu:Dock(FILL)
+			submenu:SetContents(panel)
+			submenu.Menu = self.Menu
+
+			b.SubMenu = self
+			b.SubPanel = submenu
+			submenu:SetPrevious(text, self)
+			b.DoClick = submenuclick
+			self.List:Add(b)
+
+			return submenu
+		end
+	end
+
+	--local i = 1
+	function SUBMENU:Init()
+		local topbar = self:Add("Panel")
+		topbar:Dock(TOP)
+		topbar:SetTall(40)
+
+		self.BackButton = generatebutton(" < Back")
+		self.BackButton:SetParent(topbar)
+		self.BackButton:SizeToContents()
+		self.BackButton:Dock(LEFT)
+		self.BackButton:SetContentAlignment(4)
+		self.BackButton.DoClick = submenuclick
+		self.BackButton.SubMenu = self
+		self.BackButton:Hide()
+
+		self.Contents = self:Add("DScrollPanel")
+		self.Contents:SetPaintBackground(false)
+		self.Contents:Dock(FILL)
+
+		self.List = self.Contents:Add("DListLayout")
+		self.List:Dock(FILL)
+
+		--self:AddButton("Ready up" .. i, 1, function() print("Ready!") end)
+		--i = i + 1
+		--timer.Simple(1, function() self:AddButton("Hi2", 1, function() print("nah") end) print("Added 2") end)
+
+		self:Hide()
+	end
+	function SUBMENU:SetPrevious(text, panel)
+		if IsValid(panel) then
+			self.BackButton:Show()
+			self.BackButton:SetText("  < ".. text .. " ")
+			self.BackButton:SizeToContents()
+			self.BackButton.SubPanel = panel
+		else
+			self.BackButton:Hide()
+		end
+	end
+	function SUBMENU:SetContents(panel)
+		if IsValid(self.Contents) then self.Contents:Remove() end
+		self.Contents = panel
+		panel:SetParent(self)
+		panel:Dock(FILL)
+	end
+	function SUBMENU:GetContents()
+		return self.Contents
+	end
+	vgui.Register("nzu_MenuPanel_SubMenu", SUBMENU, "Panel")
+
 	local PANEL = {}
 
 	-- Important functions for you developers out there here at the top
-	function PANEL:AddMenuButton(text, zpos, func, submenu)
-
+	function PANEL:AddButton(text, zpos, func)
+		self.MenuRoot:AddButton(text, zpos, func)
 	end
 
-	function PANEL:AddMenuSubMenu(text, zpos, submenu)
-
+	function PANEL:AddSubMenu(text, zpos)
+		return self.MenuRoot:AddSubMenu(text, zpos)
 	end
 	
-	function PANEL:AddMenuPanel(text, zpos, panel, submenu)
-
-	end
-
-	function PANEL:AddMenuSheet(text, zpos, panel)
-		local but = vgui.Create("DButton")
-		function but.Paint(s)
-			surface.SetDrawColor(0,0,0,230)
-			s:DrawFilledRect()
-		end
-		self.MenuRoot:Add(s)
-		but:SetZPos(zpos)
-		but:SetText(text)
-		but.DoClick = function(s)
-			self:ShowMenuSheet(text)
-		end
-
-		self.MenuItems[text] = panel
+	function PANEL:AddPanel(text, zpos, panel)
+		self.MenuRoot:AddPanel(text, zpos, panel)
 	end
 	
 	--function PANEL:AddNetworkedButton(text, )
@@ -55,7 +186,7 @@ if CLIENT then
 		self:Dock(FILL)
 		self.OpenTime = 0
 
-		self.LeftSide = self:Add("Panel")
+		self.LeftSide = self:Add("DDragBase") -- So buttons are clickable
 		self.LeftSide:Dock(LEFT)
 		self.LeftSide:SetWide(600)
 		self.LeftSide:DockMargin(100,100,50,100)
@@ -64,6 +195,7 @@ if CLIENT then
 		self.ConfigPanel = self.LeftSide:Add("DImage")
 		self.ConfigPanel:Dock(BOTTOM)
 		self.ConfigPanel:SetTall(337)
+		self.ConfigPanel:DockMargin(0,35,0,0)
 
 		self.ConfigPanel.InfoBar = self.ConfigPanel:Add("Panel")
 		self.ConfigPanel.InfoBar:Dock(BOTTOM)
@@ -103,11 +235,25 @@ if CLIENT then
 		self:SetConfig() -- unloaded
 
 		-- The menu list
-		self.MenuItems = {}
-		self.MenuRoot = self.LeftSide:Add("DScrollSheet")
+		self.MenuRoot = self.LeftSide:Add("nzu_MenuPanel_SubMenu")
 		self.MenuRoot:DockMargin(0,0,0,50)
 		self.MenuRoot:Dock(FILL)
+		self.MenuRoot.Menu = self
 
+		-- Now populate!
+		self:AddButton("Ready up", 1, function() print("Readying") end)
+		local configs = self:AddSubMenu("Load config ...", 2)
+			configs:AddButton("gm_construct - or something")
+			configs:AddButton("Breakout")
+			configs:AddButton("Imprisoned")
+			for i = 1,15 do
+				configs:AddButton("Demonstrating scroll: ".. i)
+			end
+			
+		local ext = vgui.Create("DPanel")
+		self:AddPanel("Extension Settings...", 3, ext)
+
+		self.MenuRoot:Show()
 		
 	end
 	
@@ -121,11 +267,14 @@ if CLIENT then
 		self:Show()
 		self:MakePopup()
 		self:SetKeyBoardInputEnabled(false)
+		self:RequestFocus()
 	end
 
 	function PANEL:Close()
 		self:Hide()
 	end
+
+	--function PANEL:DoClick() print("hfiua") end
 
 	function PANEL:Toggle()
 		if self:IsVisible() then self:Close() else self:Open() end
