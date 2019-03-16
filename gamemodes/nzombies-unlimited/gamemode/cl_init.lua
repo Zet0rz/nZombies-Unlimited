9bca18aeadce03286a10ff668105703b2aea1338
Zet0rz <zet0rmail2@gmail.com>
2019-03-16 01:30:43 +0100

    Weapon Control
    - Weapons now correctly assume slot behavior and can be switched between
    - Added WEAPON:GiveMaxAmmo() and PLAYER:GiveMaxAmmo()
    - Added support for Special Slots (needs more testing)
    - Switching weapons is now predicted
    - Switching weapons now has a max time limit in case it can't switch
    - Added PLAYER:SetWeaponLocked() and PLAYER:GetWeaponLocked(). When this is true, the player cannot switch weapons, unless the weapon has weapon.nzu_CanSpecialHolster set to true
    - Added PLAYER:SelectPreviousWeapon(), and bound it to Q
    - Removed many default HL2 HUDs
    
    With this commit, Weapon Slots almost fully works. Known bug is simply that slots that are numerically accessed but have no weapons block scroll wheel scrolling. Special slots also need testing, but othewise it is complete.

--- gamemodes/nzombies-unlimited/gamemode/cl_init.lua
+++ gamemodes/nzombies-unlimited/gamemode/cl_init.lua
@@ -1,1 +1,13 @@
 include( "shared.lua" )
+
+local hidden = {
+	CHudHealth = true,
+	CHudBattery = true,
+	--CHudAmmo = true,
+	--CHudSecondaryAmmo = true,
+	CHudWeaponSelection = true,
+}
+function GM:HUDShouldDraw(name)
+	if hidden[name] then return false end
+	return true
+end
--- gamemodes/nzombies-unlimited/gamemode/weapons.lua
+++ gamemodes/nzombies-unlimited/gamemode/weapons.lua
@@ -11,6 +11,14 @@ local function doinitialize(ply)
 		["Primary"] = primary,
 		["Secondary"] = secondary
 	}
+
+	-- DEBUG
+	timer.Simple(1, function()
+		if SERVER then
+			ply:SetNumericalAccessToWeaponSlot("test", true)
+			ply:GiveWeaponInSlot("weapon_rpg", "test")
+		end
+	end)
 end
 if SERVER then
 	hook.Add("PlayerInitialSpawn", "nzu_WeaponSlotsInit", doinitialize)
@@ -36,12 +44,16 @@ function PLAYER:GetActiveWeaponSlot()
 	return self:GetActiveWeapon():GetWeaponSlot()
 end
 function PLAYER:GetReplaceWeaponSlot()
-	for k,v in ipairs(self.nzu_WeaponSlots) do
+	for k,v in ipairs(self.nzu_WeaponSlots) do -- ipairs: Only iterate through numerical. It inherently makes it only find "Open" slots! :D
 		if not v.Weapon then
-			return k
+			return v.ID
 		end
 	end
 	return self:GetActiveWeaponSlot()
+end
+
+function PLAYER:GetMaxWeaponSlots()
+	return #self.nzu_WeaponSlots -- Only counts numerical indexes
 end
 
 
@@ -55,18 +67,85 @@ end
 
 
 
+--[[-------------------------------------------------------------------------
+Ammo Supply & Calculations (Max Ammo functions)
+---------------------------------------------------------------------------]]
+if SERVER then
+	-- This can be overwritten by any weapon
+	function WEAPON:CalculateMaxAmmo()
+		local x,y
+		if self:GetPrimaryAmmoType() >= 0 then
+			local clip = self:GetMaxClip1()
+			if clip <= 1 then
+				x = 10 -- The amount of ammo for guns that have no mags or single-shot mags
+			else
+				local upper = self.nzu_UpperPrimaryAmmo or 300
+				x = clip * math.Min(10, math.ceil(upper/clip))
+			end
+		end
+
+		if self:GetSecondaryAmmoType() >= 0 then
+			local clip = self:GetMaxClip2()
+			if clip <= 1 then
+				y = 10 -- The amount of ammo for guns that have no mags or single-shot mags
+			else
+				local upper = self.nzu_UpperSecondaryAmmo or 300
+				y = clip * math.Min(10, math.ceil(upper/clip))
+			end
+		end
+
+		return x,y
+	end
+
+	-- This can also be overwritten by any weapon
+	function WEAPON:GiveMaxAmmo()
+		local primary = self:GetPrimaryAmmoType()
+		local secondary = self:GetSecondaryAmmoType()
+		if primary >= 0 or secondary >= 0 then
+			local x,y = self:CalculateMaxAmmo()
+
+			if x and primary >= 0 then
+				local diff = x - self.Owner:GetAmmoCount(primary)
+				if diff > 0 then self.Owner:GiveAmmo(diff, primary) end
+			end
+
+			if y and secondary >= 0 then
+				local diff = y - self.Owner:GetAmmoCount(secondary)
+				if diff > 0 then self.Owner:GiveAmmo(diff, secondary) end
+			end
+		end
+	end
+
+	function PLAYER:GiveMaxAmmo()
+		for k,v in pairs(self:GetWeapons()) do
+			v:GiveMaxAmmo()
+		end
+	end
+end
+
+
+
 
 --[[-------------------------------------------------------------------------
-Adding and removing + Networking
+Weapon slots adding, removing, and networking
 ---------------------------------------------------------------------------]]
+local specialslots = {} -- Used for Special Slots (later in this file)
+
 local function doweaponslot(ply, wep, slot)
 	local wslot = ply:GetWeaponSlot(slot)
 	if not wslot then
 		wslot = {ID = slot, Weapon = wep}
 		ply.nzu_WeaponSlots[slot] = wslot
 	end
+
+	wslot.Weapon = wep
 	wep.nzu_WeaponSlot = wslot.ID
 	wep.nzu_WeaponSlot_Number = wslot.Number
+
+	if specialslots[wslot.ID] then
+		wep.OldDeploy = wep.Deploy
+		wep.Deploy = wep["nzu_SpecialDeploy"..wslot.ID] or specialslots[wslot.ID]
+	end
 end
 
 local function doremoveweapon(ply, wep)
@@ -76,12 +155,13 @@ local function doremoveweapon(ply, wep)
 	end
 end
 
-local function accessweaponslot(self, slot, b)
-	local slot = self:GetWeaponSlot(slot)
+local function accessweaponslot(self, id, b)
+	local slot = self:GetWeaponSlot(id)
 	if b then
 		if not slot then
 			slot = {}
-			self.nzu_WeaponSlots[slot] = slot
+			slot.ID = id
+			self.nzu_WeaponSlots[id] = slot
 		end
 		if not slot.Number then
 			slot.Number = table.insert(self.nzu_WeaponSlots, slot)
@@ -106,6 +186,13 @@ if SERVER then
 	util.AddNetworkString("nzu_weaponslot")
 	util.AddNetworkString("nzu_weaponslot_access")
 
+	function PLAYER:StripWeaponSlot(slot)
+		local wep = self:GetWeaponInSlot(slot)
+		if IsValid(wep) then
+			self:StripWeapon(wep:GetClass()) -- It'll auto-remove from the slot
+		end
+	end
+
 	local function doweaponslotnetwork(ply, wep, slot)
 		ply:StripWeaponSlot(slot)
 		doweaponslot(ply, wep, slot)
@@ -125,27 +212,21 @@ if SERVER then
 		net.Send(self)
 	end
 
-	function PLAYER:GiveWeaponInSlot(class, slot)
-		local wep = self:Give(class)
-		if IsValid(wep) then doweaponslotnetwork(wep, slot) end
+	function PLAYER:GiveWeaponInSlot(class, slot, noammo)
+		local wep = self:Give(class, noammo)
+		if IsValid(wep) then doweaponslotnetwork(self, wep, slot) end
 	end
 
 	hook.Add("WeaponEquip", "nzu_WeaponPickedUp", function(wep, ply)
 		timer.Simple(0, function()
 			if IsValid(wep) and IsValid(ply) and not wep.nzu_WeaponSlot then
+				print(wep)
 				local slot = ply:GetReplaceWeaponSlot()
-				doweaponslotnetwork(wep, slot)
+				doweaponslotnetwork(ply, wep, slot)
 				ply:SelectWeapon(wep:GetClass()) -- This a dumb idea with prediction?
 			end
 		end)
 	end)
-
-	function PLAYER:StripWeaponSlot(slot)
-		local wep = self:GetWeaponInSlot(slot)
-		if IsValid(wep) then
-			self:StripWeapon(wep:GetClass()) -- It'll auto-remove from the slot
-		end
-	end
 else
 	net.Receive("nzu_weaponslot", function()
 		local wep = net.ReadEntity()
@@ -161,6 +242,7 @@ else
 end
 
 
+
 --[[-------------------------------------------------------------------------
 Weapon Switching + Ammo management
 ---------------------------------------------------------------------------]]
@@ -169,40 +251,227 @@ function nzu.AddKeybindToWeaponSlot(slot, key)
 	keybinds[key] = slot
 end
 
+local maxswitchtime = 3
 hook.Add("PlayerButtonDown", "nzu_WeaponSwitching_Keybinds", function(ply, but)
 	-- Buttons 1-10 are keys 0-9
 	local slot = but < 11 and but - 1 or keybinds[but]
-	if slot then
+
+	-- What? MOUSE_WHEEL_ doesn't work even though it's within the enum??? D:
+	--[[if not slot then
+
+		print("Not numerical or keybound", but)
+		if but == MOUSE_WHEEL_UP then
+			local wep = ply:GetActiveWeapon()
+			if IsValid(wep) and wep:GetWeaponSlotNumber() then
+				slot = wep:GetWeaponSlotNumber() + 1
+				if slot > ply:GetMaxWeaponSlots() then slot = 1 end
+			end
+		elseif but == MOUSE_WHEEL_DOWN then
+			local wep = ply:GetActiveWeapon()
+			if IsValid(wep) and wep:GetWeaponSlotNumber() then
+				slot = wep:GetWeaponSlotNumber() - 1
+				if slot < 0 then slot = ply:GetMaxWeaponSlots() end
+			end
+		end
+	end]]
+
+	if but == KEY_Q then
+		ply:SelectPreviousWeapon()
+	elseif slot then
 		local wep = ply:GetWeaponInSlot(slot)
 		if IsValid(wep) then
-			ply.nzu_DoWeaponSwitch = wep
+			ply.nzu_DoSelectWeapon = wep
+			ply.nzu_DoSelectWeaponTime = CurTime() + maxswitchtime
 		end
 	end
 end)
 
-hook.Add("SetupMove", "nzu_WeaponSwitching", function(ply, mv, cmd)
-	if cmd:KeyDown(IN_WEAPON1) then -- Scroll wheel up
-		
-	elseif cmd:KeyDown(IN_WEAPON2) then -- Scroll wheel down
-		
+hook.Add("StartCommand", "nzu_WeaponSwitching", function(ply, cmd)
+	-- if PlayerButtonDown won't work, we gotta do it here :(
+	if not ply.nzu_DoSelectWeapon then
+		local m = cmd:GetMouseWheel()
+		if m ~= 0 then
+			local wep = ply:GetActiveWeapon()
+			if IsValid(wep) and wep:GetWeaponSlotNumber() then
+				local slot = wep:GetWeaponSlotNumber() + m
+
+				local max = ply:GetMaxWeaponSlots()
+				if slot > max then slot = 1 elseif slot < 1 then slot = max end
+
+				local wep2 = ply:GetWeaponInSlot(slot)
+				if IsValid(wep2) then
+					ply.nzu_DoSelectWeapon = wep2
+					ply.nzu_DoSelectWeaponTime = CurTime() + maxswitchtime
+				end
+			end
+		end
+	end
+
+	if ply.nzu_DoSelectWeapon then
+		if ply:GetActiveWeapon() == ply.nzu_DoSelectWeapon or CurTime() > ply.nzu_DoSelectWeaponTime then
+			ply.nzu_DoSelectWeapon = nil
+			ply.nzu_DoSelectWeaponTime = nil
+		else
+			cmd:SelectWeapon(ply.nzu_DoSelectWeapon)
+		end
 	end
 end)
+
+
+
+
+
+
+--[[-------------------------------------------------------------------------
+Special weapon slot behavior
+---------------------------------------------------------------------------]]
+
+nzu.AddPlayerNetworkVar("Bool", "WeaponLocked") -- When true you can't switch weapons
+
+function nzu.SpecialWeaponSlot(id, func)
+	specialslots[id] = func
+end
+
+function PLAYER:SelectPreviousWeapon()
+	local wep = self.nzu_PreviousWeapon
+	if not IsValid(wep) then wep = self:GetWeaponInSlot(1) end
+	if IsValid(wep) then
+		self.nzu_DoSelectWeapon = wep
+		self.nzu_DoSelectWeaponTime = CurTime() + maxswitchtime
+
+		-- Swap the two
+		self.nzu_PreviousWeapon = self.nzu_PreviousWeapon2
+		self.nzu_PreviousWeapon2 = wep
+	end
+end
 
 if SERVER then
 	-- Handle restoring ammo counts
 	-- This simulates separate weapon slots having separate ammo, even if they should share type
+
+	-- Also handle special weapon locking
 	function GM:PlayerSwitchWeapon(ply, old, new)
+		if ply:GetWeaponLocked() and not old.nzu_CanSpecialHolster then return true end -- Prevent switching when lock is true
+		-- Is SWEP:Holster() still called in the above?
+
 		if IsValid(old) then
 			local primary = old:GetPrimaryAmmoType()
-			if primary then old.nzu_PrimaryAmmo = ply:GetAmmoCount(primary) end
+			if primary >= 0 then old.nzu_PrimaryAmmo = ply:GetAmmoCount(primary) end
 
 			local secondary = old:GetSecondaryAmmoType()
-			if secondary then old.nzu_SecondaryAmmo = ply:GetAmmoCount(secondary) end
+			if secondary >= 0 then old.nzu_SecondaryAmmo = ply:GetAmmoCount(secondary) end
+
+			-- Store old weapons, but ONLY if they are accessible (by number)
+			if old:GetWeaponSlotNumber() then
+				ply.nzu_PreviousWeapon2 = ply.nzu_PreviousWeapon
+				ply.nzu_PreviousWeapon = old
+			end
 		end
 
 		if IsValid(new) then
 			if new.nzu_PrimaryAmmo then ply:SetAmmo(new.nzu_PrimaryAmmo, new:GetPrimaryAmmoType()) end
 			if new.nzu_SecondaryAmmo then ply:SetAmmo(new.nzu_SecondaryAmmo, new:GetSecondaryAmmoType()) end
+
+			-- Now handled with the SWEP's onboard Deploy function (making it predicted)
+			--[[local slot = new:GetWeaponSlot()
+			if slot and specialslots[slot] then
+				ply:SetWeaponLocked(true)
+				local func = new["nzu_SpecialSlot"..slot] or specialslots[slot]
+				func(new)
+			end]]
 		end
 	end
+else
+	-- Stripped down Client version only really needs to track old weapons
+	function GM:PlayerSwitchWeapon(ply, old, new)
+		if ply:GetWeaponLocked() and not old.nzu_CanSpecialHolster then return true end
+
+		if IsValid(old) then
+			if old:GetWeaponSlotNumber() then
+				ply.nzu_PreviousWeapon2 = ply.nzu_PreviousWeapon
+				ply.nzu_PreviousWeapon = old
+			end
+		end
+
+		if IsValid(new) then
+			-- Now handled with the SWEP's onboard Deploy function (making it predicted)
+			--[[local slot = new:GetWeaponSlot()
+			if slot and specialslots[slot] then
+				ply:SetWeaponLocked(true)
+				local func = new["nzu_SpecialSlot"..slot] or specialslots[slot]
+				func(new)
+			end]]
+		end
+	end
+end
+
+
+--[[-------------------------------------------------------------------------
+Populate base weapon slots
+---------------------------------------------------------------------------]]
+
+nzu.AddKeybindToWeaponSlot("Knife", KEY_V)
+if true then
+	nzu.SpecialWeaponSlot("Knife", function(self)
+		self:SetNextPrimaryFire(0)
+		self:PrimaryFire()
+		timer.Simple(0.5, function()
+			if IsValid(self) then
+				local vm = self.Owner:GetViewModel()
+				local seq = vm:GetSequence()
+				local dur = vm:SequenceDuration(seq)
+				local remaining = dur - dur*vm:GetCycle()
+				timer.Simple(remaining, function()
+					if IsValid(self) then
+						self.Owner:SetWeaponLocked(false)
+						self.Owner:SelectPreviousWeapon()
+					end
+				end)
+			end
+		end)
+	end)
+end
+
+nzu.AddKeybindToWeaponSlot("Grenade", KEY_G)
+if true then
+	nzu.SpecialWeaponSlot("Grenade", function(self)
+		self:SetNextPrimaryFire(0)
+		self:PrimaryFire()
+		timer.Simple(0.5, function()
+			if IsValid(self) then
+				local vm = self.Owner:GetViewModel()
+				local seq = vm:GetSequence()
+				local dur = vm:SequenceDuration(seq)
+				local remaining = dur - dur*vm:GetCycle()
+				timer.Simple(remaining, function()
+					if IsValid(self) then
+						self.Owner:SetWeaponLocked(false)
+						self.Owner:SelectPreviousWeapon()
+					end
+				end)
+			end
+		end)
+	end)
+end
+
+nzu.AddKeybindToWeaponSlot("SpecialGrenade", KEY_B)
+if true then
+	nzu.SpecialWeaponSlot("SpecialGrenade", function(self)
+		self:SetNextPrimaryFire(0)
+		self:PrimaryFire()
+		timer.Simple(0.5, function()
+			if IsValid(self) then
+				local vm = self.Owner:GetViewModel()
+				local seq = vm:GetSequence()
+				local dur = vm:SequenceDuration(seq)
+				local remaining = dur - dur*vm:GetCycle()
+				timer.Simple(remaining, function()
+					if IsValid(self) then
+						self.Owner:SetWeaponLocked(false)
+						self.Owner:SelectPreviousWeapon()
+					end
+				end)
+			end
+		end)
+	end)
 end
